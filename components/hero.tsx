565207df7244070f084ac99c38ad46de6e766679
Ouroborus <cesar.etam@Hotmail.com>
2024-01-04 15:32:53 -0300

    .

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,10 +28,10 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Template responsivo desenvolvido pela comunidade em Figma.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a href="https://open.cruip.com/#0" className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Template NextJS</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Template NextJS</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a href="https://vercel.com/" className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Hospedagem Vercel</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Hospedagem Vercel</a>
               </div>
             </div>
           </div>
